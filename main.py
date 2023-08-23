54951cccb1d607caf51d6fdc8e7c4d7ad975f1a4
Dev Expert <DeveExpert1879@gmail.com>
2023-08-23 07:14:18 -0500

    removed part for main function

--- main.py
+++ main.py
@@ -309,5 +309,5 @@ async def upload_pdf_file(file: UploadFile = File(...)):
 
     return data, img_ids
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
